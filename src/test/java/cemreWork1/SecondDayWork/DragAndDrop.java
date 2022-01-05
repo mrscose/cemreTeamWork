e41241769724b165b52a97146494bb6b0fb1c1f1
nuri <nursat1@hotmail.com>
2022-01-05 22:46:39 +0000

    Second Day Work

---
+++ src/test/java/cemreWork1/SecondDayWork/DragAndDrop.java
@@ -0,0 +1,17 @@
+package cemreWork1.SecondDayWork;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class DragAndDrop {
+    @Test
+    public void dragAnddROP(){
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.sybertekschole.com");
+
+
+
+
+    }
+}
