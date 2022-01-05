1adad1390ca82a45aa5bf91c79a72f3821f4582b
ahmethasanmesut <mesutyavuz34@gmail.com>
2022-01-05 22:00:35 +0000

    work has been done

---
+++ src/test/java/cemreWork1/work0105.java
@@ -0,0 +1,7 @@
+package cemreWork1;
+
+public class work0105 {
+    public static void main(String[] args) {
+        System.out.println("mesut was also  here");
+    }
+}
--- src/test/java/cemreWork1/work1.java
+++ src/test/java/cemreWork1/work1.java
@@ -1,2 +1,5 @@
 package cemreWork1;public class work1 {
+    public static void main(String[] args) {
+        System.out.println("mesut was here");
+    }
 }
