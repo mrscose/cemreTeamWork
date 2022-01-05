5541349f33f710dfc04a7701a7afa77fcc5b0a8b
nurcanuk <enurcan2010@gmail.com>
2022-01-05 22:57:08 +0000

    Nurcan2's work 3 done

--- src/test/java/Nurcan2.java
+++ src/test/java/Nurcan2.java
@@ -3,5 +3,6 @@ public class Nurcan2 {
     public static void main(String[] args) {
         System.out.println("Nurcan2's work");
         System.out.println("Hello");
+        System.out.println("Cybertek");
     }
 }
